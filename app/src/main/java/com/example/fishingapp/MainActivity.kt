14f737597e658ea59aa6b316066e339656bbb64c
Xi (Michael) CHen <chen1881@purdue.edu>
2019-03-22 10:12:25 -0400

    Test sample

--- app/src/main/java/com/example/fishingapp/MainActivity.kt
+++ app/src/main/java/com/example/fishingapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
+//Test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
